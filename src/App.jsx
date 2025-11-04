d6dc248d3676c962869a7166d057df7e5bf8d9aa
mxdina01 <dika121.medina@gmail.com>
2025-11-04 17:11:21 -0300

    Actualización de componentes y páginas para Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./components/styles/app.css";
+import "./components/styles/App.css";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Pages/Home";
