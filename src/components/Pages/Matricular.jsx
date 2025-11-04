9f0c3ada9f5181a390654323b83ebccc8eb561ed
mxdina01 <dika121.medina@gmail.com>
2025-11-04 20:41:57 -0300

    Update Matricular.jsx

--- src/components/Pages/Matricular.jsx
+++ src/components/Pages/Matricular.jsx
@@ -73,7 +73,7 @@ function Matricular() {
     }
 
     try {
-      await fetch(`https://psis-2025.onrender.com/api/matriculas`, {
+      await fetch(`https://psis-2025.onrender.com/api/matricular`, {
         method: "POST",
         headers: { "Content-Type": "application/json",
             "Authorization": `Bearer ${AuthService.getToken()}`
