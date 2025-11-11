37dc280727643896d835474ea520e9150bda8ece
mxdina01 <dika121.medina@gmail.com>
2025-11-11 10:14:45 -0300

    otro cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./components/styles/App.css";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation, matchPath } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Pages/Home";
 import AgregarAlumno from "./components/Pages/AgregarAlumno";
@@ -16,42 +16,40 @@ import EditarAlumno from "./components/Pages/EditarAlumno";
 const routes = [
   { path: "/", element: <Login />, hideHeader: true },
   { path: "/login", element: <Login />, hideHeader: true },
-  { path: "/register", element: <Register />, hideHeader: true},
-  { path: "/home", element: <Home />, hideHeader: false},
-  { path: "/alumnos", element: <Alumnos />, hideHeader: false},
-  { path: "/agregaralumno", element: <AgregarAlumno/>, hideHeader: false},
-  {path: "/editaralumno/:id", element: <EditarAlumno/>, hideHeader:false},
-  { path: "/cursos", element: <Cursos/>, hideHeader: false},
-  { path: "/matricular", element: <Matricular/>, hideHeader: false},
+  { path: "/register", element: <Register />, hideHeader: true },
+  { path: "/home", element: <Home />, hideHeader: false },
+  { path: "/alumnos", element: <Alumnos />, hideHeader: false },
+  { path: "/agregaralumno", element: <AgregarAlumno />, hideHeader: false },
+  { path: "/editaralumno/:id", element: <EditarAlumno />, hideHeader: false },
+  { path: "/cursos", element: <Cursos />, hideHeader: false },
+  { path: "/matricular", element: <Matricular />, hideHeader: false },
+];
 
-]
-
-function AppContent(){
-
-//fetchea en cual ubi se encuentra el user currently
+function AppContent() {
+  //fetchea en cual ubi se encuentra el user currently
   const location = useLocation();
 
-//busca la ruta actual een nuestra lista y ve si debemos ocultar el header
-const route = routes.find(r =>r.path === location.pathname);
-const hideHeader = route?.hideHeader;
-return (
-  <>
-    {!hideHeader && <Header />}
-    <Routes>
-      {/* renderizado dinamico de rutass */}
-      {routes.map(r => (
-        <Route key={r.path} path={r.path} element={r.element}/>
-      ))}
-    </Routes>
-  </>
-);
+  //busca la ruta actual en nuestra lista y ve si debemos ocultar el header
+  const route = routes.find(r => matchPath({ path: r.path, end: true }, location.pathname));
+  const hideHeader = route?.hideHeader;
 
+  return (
+    <>
+      {!hideHeader && <Header />}
+      <Routes>
+        {/* renderizado dinamico de rutas */}
+        {routes.map(r => (
+          <Route key={r.path} path={r.path} element={r.element} />
+        ))}
+      </Routes>
+    </>
+  );
 }
-    
-  export default function App(){
-    return (
-      <Router>
-        <AppContent />
-      </Router>
-    );
-  }
+
+export default function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
